a7bbda640f795b3041151c605e74b74b08cff0a2
GWC 523 <gwynethchiu15@gmail.com>
2022-05-16 18:43:03 +0800

    Added basic navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-     Hello World!
+      <Navbar />
+      <div className="container">
+        <article>
+          <h1>What is Lorem Ipsum? </h1>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry...
+        </article>
+      </div>
     </div>
   );
 }
