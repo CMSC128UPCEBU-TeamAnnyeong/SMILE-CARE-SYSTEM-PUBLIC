172c9d2c6a76f8c708289a603414c2305a874148
GWC 523 <gwynethchiu15@gmail.com>
2022-05-16 17:41:40 +0800

    Added structure for the project

---
+++ src/Helpers/Utils/Common.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import axios from 'axios';
+import { logoutUser } from '../apiCalls/authApi';
+
+//Common Utility Functions
+
+export const refreshPage = () => {
+    window.location.reload();
+}
+
+export const getTime = (date) => {
+    return  date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+}
+
+export const formatDate = (date) => {
+    var formatDate = new Date(date);
+    var stringDate = formatDate.toDateString().split(" ");
+
+    return stringDate[1] + " " + stringDate[2] + ", " + stringDate[3]
+}
+
+
+/***************************
+ * Local Storage Utilities
+ ***************************/
+
+
+//return user data from local storage
+export const getUser = () => {
+    const userStr = localStorage.getItem('user');
+    if(userStr) return JSON.parse(userStr);
+    else return null;
+}
+
+//return user data from local storage
+export const getUserId = () => {
+    const idStr = localStorage.getItem('id');
+    if(idStr) return JSON.parse(idStr);
+    else return null;
+}
+
+//return role id from local storage
+export const getRoleId = () => {
+    return localStorage.getItem('role_id') || null;
+}
+
+//return token from local storage
+export const getToken = () => {
+    return localStorage.getItem('token') || null;
+}
+
+//return token expiry from local storage
+export const getTokenExpiry = () => {
+    return localStorage.getItem('token_expiry') || null;
+}
+
+//log logout session
+export const logOut = () => {
+    axios({
+        method: 'post',
+        url: window.$link + 'logout/index/' + window.$userId,
+        withCredentials: false, 
+        params: {
+            api_key: window.$api_key,
+        }
+    }).then(function (message) {
+    });
+}
+
+//remove token from local storage
+export const removeUserSession = () => {
+    logoutUser();
+    localStorage.removeItem('user');
+    refreshPage();
+}
+
+//set the token and user from local storage
+export const setUserSession = (token, user) => {
+    localStorage.setItem('token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+}
