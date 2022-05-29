f98e966d338835e371594ed1d942617fdae6b3d7
GWC 523 <gwynethchiu15@gmail.com>
2022-05-29 23:56:34 +0800

    Updates

--- src/Components/FontAwesomeIcons/index.js
+++
@@ -1,4 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faGreaterThan, faLessThan, faArrowRight, faArrowLeft, faEnvelope, faCaretDown, faCaretUp, faWindowClose, faPrint, faEye, faMinusSquare, faEyeSlash, faSearch, faTimes, faBars, faUser, faLock, faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
-
-library.add( faGreaterThan, faLessThan, faArrowRight, faArrowLeft, faEnvelope, faCaretDown, faCaretUp, faWindowClose, faPrint, faMinusSquare, faEye, faEyeSlash, faSearch, faTimes, faBars, faUser, faLock, faCheckSquare, faCoffee)
