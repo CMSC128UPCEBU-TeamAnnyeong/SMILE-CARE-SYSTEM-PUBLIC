48d9911ba4566232b51d7a03d03ba1d7c41e1343
Alberto A. Cabrera Jr <alberto@Alberto-Mac.local>
2022-06-02 19:19:16 +0800

    Fix spelling

--- src/Components/PoweredBy/PoweredBy.js
+++ src/Components/PoweredBy/PoweredBy.js
@@ -5,7 +5,7 @@ import logoSmileCareSystem from "../../Assets/logo.png"
 function PoweredBy() {
   return (
     <div className="footer">
-      <div className="poweredby-text">POWERED BYYYY</div>
+      <div className="poweredby-text">POWERED BY</div>
       <img src={logoSmileCareSystem} className="poweredby-logo" />
     </div>
   )
