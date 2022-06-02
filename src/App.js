7fbd3983172f5adef84b031295094a2eac7b2457
Alberto A. Cabrera Jr <alberto@Alberto-Mac.local>
2022-06-02 19:08:19 +0800

    Deploy

--- src/App.js
+++ src/App.js
@@ -1,40 +1,47 @@
-import React, { useState } from 'react';
-import './App.css'
-import './Components/FontAwesomeIcons';
-import Navbar from './Components/Navbar/Navbar';
-import PoweredBy from './Components/PoweredBy/PoweredBy';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { getUser } from './Helpers/Utils/Common';
+import React, { useState } from "react"
+import "./App.css"
+import "./Components/FontAwesomeIcons"
+import Navbar from "./Components/Navbar/Navbar"
+import PoweredBy from "./Components/PoweredBy/PoweredBy"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom"
+import { getUser } from "./Helpers/Utils/Common"
 
 /** PAGES **/
-import Home from './Pages/Home'
-import ViewQueue from './Pages/ViewQueue';
-import ViewQueueTable from './Pages/ViewQueueTable';
-import BookAppointment from './Pages/BookAppointment';
-import BookAppointmentForm from './Components/BookAppointmentForm/BookAppointmentForm';
-import Contact from './Pages/Contact';
+import Home from "./Pages/Home"
+import ViewQueue from "./Pages/ViewQueue"
+import ViewQueueTable from "./Pages/ViewQueueTable"
+import BookAppointment from "./Pages/BookAppointment"
+import BookAppointmentForm from "./Components/BookAppointmentForm/BookAppointmentForm"
+import Contact from "./Pages/Contact"
 
 function App() {
-
-  document.title = 'SMILE CARE SYSTEM';
-  const [isAuthenticated, setAuthentication] = useState(getUser());
+  document.title = "SMILE CARE SYSTEM"
+  const [isAuthenticated, setAuthentication] = useState(getUser())
 
   return (
     <div className="App">
       <Navbar />
-      {/* <Router> */}
+      <Router>
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/book-appointment-form" element={<BookAppointmentForm/>}/> 
-          <Route path="/view" element={<ViewQueue/>}/>
-          <Route path="/view/queue" element={<ViewQueueTable/>}/>
-          <Route path="/appointment" element={<BookAppointment/>}/>
-          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/book-appointment-form"
+            element={<BookAppointmentForm />}
+          />
+          <Route path="/view" element={<ViewQueue />} />
+          <Route path="/view/queue" element={<ViewQueueTable />} />
+          <Route path="/appointment" element={<BookAppointment />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
-      {/* </Router> */}
+      </Router>
       <PoweredBy />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
