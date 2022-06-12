f17458540cdada19bee2fae3ae53b798255f3f8d
Alberto A. Cabrera Jr <alberto@Alberto-Mac.local>
2022-06-13 00:47:35 +0800

    fsef

--- src/Pages/Appointments/Returning Patient/ReturningPatientForm1.js
+++ src/Pages/Appointments/Returning Patient/ReturningPatientForm1.js
@@ -9,7 +9,10 @@ import {
 import { createAppointment } from "../../../Helpers/apiCalls/appointmentApi"
 import { ToastContainer, toast } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
-import { getPatient } from "../../../Helpers/apiCalls/patientApi"
+import {
+  getPatient,
+  getPatientByShortId
+} from "../../../Helpers/apiCalls/patientApi"
 import { useParams } from "react-router-dom"
 
 //css
@@ -36,9 +39,9 @@ function ReturningPatientForm1({
   const [serviceOptions, setServiceOptions] = useState([])
   const [redirect, setRedirect] = useState(false)
   const [patient, setPatient] = useState({})
-  const [purpose, setPurpose] = useState("");
-  const [patientId, setPatientId] = useState("");
-  const [isFound, setIsFound] = useState(false);
+  const [purpose, setPurpose] = useState("")
+  const [patientId, setPatientId] = useState("")
+  const [isFound, setIsFound] = useState(false)
   const { id } = useParams()
 
   const options = [{ label: "Root Canal", value: "" }]
@@ -74,10 +77,10 @@ function ReturningPatientForm1({
   }
 
   async function fetchPatient(id) {
-    const response = await getPatient(id)
+    const response = await getPatientByShortId(id)
     console.log(response)
-    if(response.data.status === 200) {
-      setIsFound(true);
+    if (response.data.status === 200) {
+      setIsFound(true)
       setPatient(response.data.data.data)
     }
   }
@@ -155,11 +158,10 @@ function ReturningPatientForm1({
     <div className="page">
       <ToastContainer />
       <div className={`container ${inactive ? "inactive" : "active"}`}>
-        <div className="row">
-        </div>
+        <div className="row"></div>
         <div className="row page-content">
           <div className="form-card-cont">
-          <div className="row mt-4">
+            <div className="row mt-4">
               <div className="col-sm-4">
                 <span className="form-label">Patient ID</span>
                 <br />
@@ -174,62 +176,67 @@ function ReturningPatientForm1({
                 </div>
               </div>
               <div className="col-sm-6">
-                  <button className="form-button-search" onClick={() => fetchPatient(patientId)}>Search</button>
-                </div>
+                <button
+                  className="form-button-search"
+                  onClick={() => fetchPatient(patientId)}
+                >
+                  Search
+                </button>
+              </div>
             </div>
             {isFound === true && (
-                <>
+              <>
                 <div className="row mt-4">
-                <div className="col-sm-4">
-                  <span className="form-label">Last Name</span>
-                  <br />
-                  <div className="input-cont">
-                    <input
-                      type="text"
-                      data-testid="last-name-input"
-                      className="form-input last-name-input"
-                      name="lastName"
-                      value={patient.last_name}
-                      onChange={setPatient}
-                      readOnly
-                      disabled
-                    />
+                  <div className="col-sm-4">
+                    <span className="form-label">Last Name</span>
+                    <br />
+                    <div className="input-cont">
+                      <input
+                        type="text"
+                        data-testid="last-name-input"
+                        className="form-input last-name-input"
+                        name="lastName"
+                        value={patient.last_name}
+                        onChange={setPatient}
+                        readOnly
+                        disabled
+                      />
+                    </div>
+                  </div>
+                  <div className="col-sm-4">
+                    <span className="form-label">First Name</span>
+                    <br />
+                    <div className="input-cont">
+                      <input
+                        type="text"
+                        className="form-input first-name-input"
+                        name="firstName"
+                        value={patient.first_name}
+                        onChange={setPatient}
+                        readOnly
+                        disabled
+                      />
+                    </div>
+                  </div>
+                  <div className="col-sm-4">
+                    <span className="form-label">Middle Name</span>
+                    <br />
+                    <div className="input-cont">
+                      <input
+                        type="text"
+                        className="form-input middle-name-input"
+                        name="middleName"
+                        value={patient.middle_name}
+                        onChange={setPatient}
+                        readOnly
+                        disabled
+                      />
+                    </div>
                   </div>
                 </div>
-                <div className="col-sm-4">
-                  <span className="form-label">First Name</span>
-                  <br />
-                  <div className="input-cont">
-                    <input
-                      type="text"
-                      className="form-input first-name-input"
-                      name="firstName"
-                      value={patient.first_name}
-                      onChange={setPatient}
-                      readOnly
-                      disabled
-                    />
-                  </div>
-                </div>
-                <div className="col-sm-4">
-                  <span className="form-label">Middle Name</span>
-                  <br />
-                  <div className="input-cont">
-                    <input
-                      type="text"
-                      className="form-input middle-name-input"
-                      name="middleName"
-                      value={patient.middle_name}
-                      onChange={setPatient}
-                      readOnly
-                      disabled
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="row mt-2">
-              <div className="col-sm-4">
-                  <span className="form-label">Street Address</span>
+                <div className="row mt-2">
+                  <div className="col-sm-4">
+                    <span className="form-label">Street Address</span>
                     <br />
                     <div className="input-cont">
                       <input
@@ -242,97 +249,95 @@ function ReturningPatientForm1({
                         disabled
                       />
                     </div>
-                </div>
-                <div className="col-sm-4">
-                  <span className="form-label">Appointment Date Time</span>
-                  <br />
-                  <DateTimePicker
-                    className="form-input appointmentdatetime-input"
-                    onChange= {setAppointment}
-                    value={appointment}
-                  />
-                </div>
-                <div className="col-sm-4">
-                  <span className="form-label">Purpose</span>
-                  <br />
-                  <select
-                    className="form-input category-input"
-                    name="services"
-                    onChange={(e) => setPurpose(e.target.value)}
-                  >
-                    <option value=""> Select </option>
-                    <option value="check-up">Check-up</option>
-                    <option value="follow-up">Follow-up</option>
-                    <option value="others">Others</option>
-                    {serviceOptions.map((data, index) => {
-                      return <option value={data.label}>{data.label}</option>
-                    })}
-                  </select>
-                </div>
-              </div>
-              <div className="row mt-2 personal-input-cont">
-                <div className="col-sm-4">
-                  <span className="form-label">City</span>
-                  <br />
-                  <div className="input-cont">
-                    <input
-                      type="text"
-                      className="form-input city-input"
-                      name="city"
-                      value={patient.city}
-                      onChange={setPatient}
-                      readOnly
-                      disabled
-                    />
                   </div>
-                  <span className="form-label">Email Address</span>
-                  <br />
-                  <div className="input-cont">
-                    <input
-                      type="text"
-                      className="form-input email-address-input"
-                      name="emailAddress"
-                      value={patient.email}
-                      onChange={setPatient}
-                      readOnly
-                      disabled
-                    />
-                  </div>
-                  <span className="form-label">Mobile Number</span>
-                  <br />
-                  <div className="input-cont">
-                    <input
-                      type="text"
-                      className="form-input mobile-number-input"
-                      name="mobileNumber"
-                      value={patient.mobile_number}
-                      onChange={setPatient}
-                      readOnly
-                      disabled
-                    />
-                  </div>
-                </div>
-                <div class="col-sm-4">
-                  <div className="notes-cont">
-                    <span className="form-label"> Notes</span>
+                  <div className="col-sm-4">
+                    <span className="form-label">Appointment Date Time</span>
                     <br />
-                    <textarea
-                      id="notes"
-                      name="remarks"
-                      className="remarks"
-                      rows="9"
-                      cols="90"
-                      value={notes}
-                      onChange={(e) => setNotes(e.target.value)}
+                    <DateTimePicker
+                      className="form-input appointmentdatetime-input"
+                      onChange={setAppointment}
+                      value={appointment}
                     />
                   </div>
+                  <div className="col-sm-4">
+                    <span className="form-label">Purpose</span>
+                    <br />
+                    <select
+                      className="form-input category-input"
+                      name="services"
+                      onChange={(e) => setPurpose(e.target.value)}
+                    >
+                      <option value=""> Select </option>
+                      <option value="check-up">Check-up</option>
+                      <option value="follow-up">Follow-up</option>
+                      <option value="others">Others</option>
+                      {serviceOptions.map((data, index) => {
+                        return <option value={data.label}>{data.label}</option>
+                      })}
+                    </select>
+                  </div>
                 </div>
-              </div>
+                <div className="row mt-2 personal-input-cont">
+                  <div className="col-sm-4">
+                    <span className="form-label">City</span>
+                    <br />
+                    <div className="input-cont">
+                      <input
+                        type="text"
+                        className="form-input city-input"
+                        name="city"
+                        value={patient.city}
+                        onChange={setPatient}
+                        readOnly
+                        disabled
+                      />
+                    </div>
+                    <span className="form-label">Email Address</span>
+                    <br />
+                    <div className="input-cont">
+                      <input
+                        type="text"
+                        className="form-input email-address-input"
+                        name="emailAddress"
+                        value={patient.email}
+                        onChange={setPatient}
+                        readOnly
+                        disabled
+                      />
+                    </div>
+                    <span className="form-label">Mobile Number</span>
+                    <br />
+                    <div className="input-cont">
+                      <input
+                        type="text"
+                        className="form-input mobile-number-input"
+                        name="mobileNumber"
+                        value={patient.mobile_number}
+                        onChange={setPatient}
+                        readOnly
+                        disabled
+                      />
+                    </div>
+                  </div>
+                  <div class="col-sm-4">
+                    <div className="notes-cont">
+                      <span className="form-label"> Notes</span>
+                      <br />
+                      <textarea
+                        id="notes"
+                        name="remarks"
+                        className="remarks"
+                        rows="9"
+                        cols="90"
+                        value={notes}
+                        onChange={(e) => setNotes(e.target.value)}
+                      />
+                    </div>
+                  </div>
+                </div>
               </>
             )}
-            {isFound === true && (
-                proceed()
-            )}
+            {isFound === true && proceed()}
           </div>
         </div>
       </div>
