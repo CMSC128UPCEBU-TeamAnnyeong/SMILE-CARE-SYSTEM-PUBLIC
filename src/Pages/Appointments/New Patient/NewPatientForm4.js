fc5895be6361c7395428e62e21139ab7ffbbb034
ditismaywose <mayrosepasasadaba@gmail.com>
2022-06-13 13:36:30 +0800

    Added New Patient Last Page and Redirection

--- src/Pages/Appointments/New Patient/NewPatientForm4.js
+++ src/Pages/Appointments/New Patient/NewPatientForm4.js
@@ -10,6 +10,7 @@ import { createAppointment } from "../../../Helpers/apiCalls/appointmentApi"
 import { ToastContainer, toast } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 import { Navigate } from "react-router-dom"
+import ProfileUpload from "./ProfileUpload/ProfileUpload"
 
 //css
 import "./newPatient.css"
@@ -37,6 +38,8 @@ function NewPatientForm4({
   const [serviceOptions, setServiceOptions] = useState([])
   const [redirect, setRedirect] = useState(false)
   const [purpose, setPurpose] = useState("")
+  const [profilePicture, setProfilePicture] = useState("")
+
 
   const options = [{ label: "Root Canal", value: "" }]
 
@@ -49,10 +52,14 @@ function NewPatientForm4({
       sickness,
       agreement
     )
+    console.log(response)
     if (response) {
-      console.log(response)
       toast.success("Successfully Added Patient!")
+      setTimeout(function () {
+        setRedirect(true)
+      }, 2000)
       const id = response.data.data.id
+      console.log(id)
       submitAppointment(id)
     } else if (response.error) {
       toast.error(response.error)
@@ -75,12 +82,13 @@ function NewPatientForm4({
     )
     console.log(response)
     if (response) {
-      toast.success("Successfully Added Appointment!")
-      setTimeout(function () {
-        setRedirect(true)
-      }, 2000)
+      toast.success("Successfully Added Appointment!")    // This never shows up idk why
+      // setTimeout(function () {
+      //   setRedirect(true)
+      // }, 2000)
     }
   }
+
 
   function proceed() {
     if (appointmentDateTime != "" /*&& services.length != 0*/) {
@@ -122,57 +130,81 @@ function NewPatientForm4({
     fetchServices()
   }, [category])
 
+  console.log(redirect)
   if (redirect == true) {
-    return <Navigate to={"/appointments"} />
+    return <Navigate to={"/appointment"} />
   }
 
   return (
     <div className="page">
       <ToastContainer />
       <div className={`container ${inactive ? "inactive" : "active"}`}>
+        <div className="row">
+          <div className="col-sm-6">
+            <h1 className="page-title">Add Appointment</h1>
+          </div>
+        </div>
         <div className="row page-content">
           <div className="form-card-cont">
-            <div className="row mt-4">
+            <div className="row">
               <div className="col-sm-4">
-                <span className="form-label">Appointment Date Time</span>
-                <br />
-                <DateTimePicker
-                  className="form-input"
-                  onChange={onChange}
-                  value={value}
+                <div>
+                  <h1 className="form-label" style={{ paddingTop: "1%" }}>
+                    Profile Picture
+                  </h1>
+                </div>
+                <ProfileUpload
+                  setProfilePicture={setProfilePicture}
+                  profilePicture={profilePicture}
                 />
               </div>
-               <div className="col-sm-4">
-                <span className="form-label">Purpose</span>
-                <br />
-                <select
-                  className="form-input category-input"
-                  name="purpose"
-                  onChange={(e) => setPurpose(e.target.value)}
-                >
-                  <option value="" disabled> Select </option>
-                  <option value="check-up">Check-up</option>
-                  <option value="follow-up">Follow-up</option>
-                  <option value="others">Others</option>
-                  {serviceOptions.map((data, index) => {
-                    return <option value={data.label}>{data.label}</option>
-                  })}
-                </select>
+              <div className="col-sm-8">
+                <div className="row mb-4">
+                  <div className="col">
+                    <span className="form-label">Appointment Date Time</span>
+                    <br />
+                    <DateTimePicker
+                      className="form-input"
+                      onChange={onChange}
+                      value={value}
+                    />
+                  </div>
+                  <div className="col">
+                    <span className="form-label">Purpose</span>
+                    <br />
+                    <select
+                      className="form-input category-input"
+                      name="services"
+                    >
+                      <option value="" disabled>
+                        {" "}
+                        Select{" "}
+                      </option>
+                      <option value="check-up">Check-up</option>
+                      <option value="follow-up">Follow-up</option>
+                      <option value="others">Others</option>
+                      {serviceOptions.map((data, index) => {
+                        return <option value={data.label}>{data.label}</option>
+                      })}
+                    </select>
+                  </div>
+                </div>
+                <div className="row">
+                  <span className="form-label">Remarks / Notes</span>
+                  <br />
+                  <textarea
+                    id="notes"
+                    name="notes"
+                    className="notesNewPatient"
+                    rows="10"
+                    cols="115"
+                    value={notes}
+                    onChange={(e) => setNotes(e.target.value)}
+                  />
+                </div>
               </div>
             </div>
-            <div class="row mt-5">
-              <span className="form-label">Remarks / Notes</span>
-              <br />
-              <textarea
-                id="notes"
-                name="notes"
-                className="notesNewPatient"
-                rows="4"
-                cols="117"
-                value={notes}
-                onChange={(e) => setNotes(e.target.value)}
-              />
-            </div>
+
             <div className="row">
               <div className="col d-flex justify-content-end">
                 <button
