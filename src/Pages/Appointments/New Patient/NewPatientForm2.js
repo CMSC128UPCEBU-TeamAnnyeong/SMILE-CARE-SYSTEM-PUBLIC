7f4cc0098534ba79067bf96ae7f5944f1c23b938
Alberto A. Cabrera Jr <alberto@Alberto-Mac.local>
2022-06-14 19:24:22 +0800

    Mobile responsive

--- src/Pages/Appointments/New Patient/NewPatientForm2.js
+++ src/Pages/Appointments/New Patient/NewPatientForm2.js
@@ -26,375 +26,207 @@ function NewPatientForm2({
     isAllergictoMentionedDrugs
   } = medicalHistory
 
-  function proceed() {
-    if (
-      inGoodHealth !== "" &&
-      isUnderTreatment !== "" &&
-      hasIllnessOrSurgery !== "" &&
-      isHospitalized !== "" &&
-      isTakingPrescription !== "" &&
-      isUsingTabacco !== "" &&
-      isUsingDangerousDrugs !== "" &&
-      isAllergictoMentionedDrugs !== ""
-    ) {
-      return (
-        <div className="row">
-          <div className="col d-flex justify-content-end">
-            <button
-              className="form-button-previous"
-              onClick={() => navigation.previous()}
-            >
-              BACK
-            </button>
-            <button
-              className="form-button-next"
-              onClick={() => navigation.next()}
-            >
-              NEXT
-            </button>
+  const renderPreviousPhysicianField = ({ title, name, value, onChange }) => {
+    return (
+      <div className="previous-physician-field">
+        <div className="previous-physician-field-title">{title}</div>
+        <input
+          type="text"
+          className="previous-physician-field-input"
+          name={name}
+          value={value}
+          onChange={onChange}
+        />
+      </div>
+    )
+  }
+
+  const renderPreviousPhysicianFields = () => {
+    return (
+      <div className="previous-physician-fields">
+        {renderPreviousPhysicianField({
+          title: "Name of Physician",
+          name: "physician",
+          value: physician,
+          onChange: setMedicalHistory
+        })}
+        {renderPreviousPhysicianField({
+          title: "Office Address",
+          name: "physicianOfficeAddress",
+          value: physicianOfficeAddress,
+          onChange: setMedicalHistory
+        })}
+        {renderPreviousPhysicianField({
+          title: "Specialty",
+          name: "physicianSpecialty",
+          value: physicianSpecialty,
+          onChange: setMedicalHistory
+        })}
+      </div>
+    )
+  }
+
+  const renderMedicalHistoryInformationSection = () => {
+    return (
+      <div className="section">
+        <div className="section-title">Previous Physician</div>
+        <div className="section-body">{renderPreviousPhysicianFields()}</div>
+      </div>
+    )
+  }
+
+  const renderCurrentConditionSection = () => {
+    return (
+      <div className="section">
+        <div className="section-title">Current Condition</div>
+        <div className="section-body">
+          {renderQuestionField({
+            name: "inGoodHealth",
+            question: "Are you in good health?",
+            value: inGoodHealth,
+            onChange: setMedicalHistory
+          })}
+          {renderQuestionField({
+            name: "isUnderTreatment",
+            question: "Are you under treatment now?",
+            value: isUnderTreatment,
+            onChange: setMedicalHistory,
+            specificDetails: {
+              placeholder: "Condition Being Treated"
+            }
+          })}
+          {renderQuestionField({
+            name: "hasIllnessOrSurgery",
+            question:
+              "Have you ever had serious illness or surgical operation?",
+            value: hasIllnessOrSurgery,
+            onChange: setMedicalHistory
+          })}
+          {renderQuestionField({
+            name: "isHospitalized",
+            question: "Have you ever been hospitalize?",
+            value: isHospitalized,
+            onChange: setMedicalHistory
+          })}
+          {renderQuestionField({
+            name: "isTakingPrescription",
+            question:
+              "Are you taking any prescription/non-prescription medication?",
+            value: isTakingPrescription,
+            onChange: setMedicalHistory,
+            specificDetails: {
+              placeholder: "Please specify"
+            }
+          })}
+          {renderQuestionField({
+            name: "isUsingTabacco",
+            question: "Do you use Tabacco products?",
+            value: isUsingTabacco,
+            onChange: setMedicalHistory
+          })}
+          {renderQuestionField({
+            name: "isUsingDangerousDrugs",
+            question: "Do you use alcohol, cocaine or other dangerous drugs?",
+            value: isUsingDangerousDrugs,
+            onChange: setMedicalHistory
+          })}
+          {renderQuestionField({
+            name: "isAllergictoMentionedDrugs",
+            question:
+              "Are you allergic to any of the following (Local Anesthetic, Penicillin, Antibiotics, Sulfa drugs, Aspirin, Latex, Others...)?",
+            value: isAllergictoMentionedDrugs,
+            onChange: setMedicalHistory,
+            specificDetails: {
+              placeholder: "Please specify"
+            }
+          })}
+        </div>
+      </div>
+    )
+  }
+
+  const renderQuestionField = ({
+    name,
+    question,
+    value,
+    onChange,
+    specificDetails = null
+  }) => {
+    return (
+      <div className="question-field">
+        <div className="question-section">
+          <div className="question">{question}</div>
+          <div className="choices">
+            <div className="choice">
+              <input
+                type="radio"
+                name={name}
+                value="yes"
+                checked={value === "yes"}
+                onChange={onChange}
+              />
+              <div className="text">Yes</div>
+            </div>
+            <div className="choice">
+              <input
+                type="radio"
+                name={name}
+                value="no"
+                checked={value === "no"}
+                onChange={onChange}
+              />
+              <div className="text">No</div>
+            </div>
           </div>
         </div>
-      )
+        {specificDetails && value === "yes" && (
+          <div className="specific-details-section">
+            <input
+              type="text"
+              onChange={onChange}
+              name="specifiedTreatment"
+              placeholder={specificDetails.placeholder}
+            />
+          </div>
+        )}
+      </div>
+    )
+  }
+
+  const renderButtons = () => {
+    if (
+      !inGoodHealth &&
+      !isUnderTreatment &&
+      !hasIllnessOrSurgery &&
+      !isHospitalized &&
+      !isTakingPrescription &&
+      !isUsingTabacco &&
+      !isUsingDangerousDrugs &&
+      !isAllergictoMentionedDrugs
+    ) {
+      return
     }
+
+    return (
+      <div className="buttons">
+        <button className="button back" onClick={() => navigation.previous()}>
+          BACK
+        </button>
+        <button className="button next" onClick={() => navigation.next()}>
+          NEXT
+        </button>
+      </div>
+    )
   }
 
   return (
     <div className="page">
       <div className={`container ${inactive ? "inactive" : "active"}`}>
-        <div className="row page-content">
-          <div className="medical-form-card-cont">
-            <div className="row">
-              <h1 className="medical-info-header">
-                Medical History Information
-              </h1>
-            </div>
-            <div className="row mt-4">
-              <span className="physician-label">Previous Physician</span>
-              <br />
-            </div>
-            <div className="row mt-4">
-              <div className="col-sm-4">
-                <span className="medical-form-label">Name of Physician</span>
-                <br />
-                <input
-                  type="text"
-                  className="form-input last-name-input"
-                  name="physician"
-                  value={physician}
-                  onChange={setMedicalHistory}
-                />
-              </div>
-              <div className="col-sm-4">
-                <span className="medical-form-label">Office Address</span>
-                <br />
-                <input
-                  type="text"
-                  className="form-input first-name-input"
-                  name="physicianOfficeAddress"
-                  value={physicianOfficeAddress}
-                  onChange={setMedicalHistory}
-                />
-              </div>
-              <div className="col-sm-4">
-                <span className="medical-form-label">Specialty</span>
-                <br />
-                <input
-                  type="text"
-                  className="form-input middle-name-input"
-                  name="physicianSpecialty"
-                  value={physicianSpecialty}
-                  onChange={setMedicalHistory}
-                />
-              </div>
-            </div>
-            <div className="row mt-4">
-              <span className="current-condition">Current Condition</span>
-              <br />
-            </div>
-            <div className="row mt-4">
-              <div className="col-sm-8">
-                <p className="questions-1">Are you in good health?</p>
-                <br />
-              </div>
-              <div className="col-sm-1">
-                <div className="response-yes-1">
-                  <input
-                    type="radio"
-                    name="inGoodHealth"
-                    value="yes"
-                    checked={inGoodHealth === "yes"}
-                    onChange={setMedicalHistory}
-                  />
-                  Yes
-                </div>
-              </div>
-              <div className="col-sm-1">
-                <div className="response-no-1">
-                  <input
-                    type="radio"
-                    name="inGoodHealth"
-                    value="no"
-                    checked={inGoodHealth === "no"}
-                    onChange={setMedicalHistory}
-                  />
-                  No
-                </div>
-              </div>
-            </div>
-            <div className="row mt-4">
-              <div className="col-sm-8">
-                <p className="questions-2">Are you under treatment now?</p>
-                <br />
-              </div>
-              <div className="col-sm-1">
-                <div className="response-yes-2">
-                  <input
-                    type="radio"
-                    name="isUnderTreatment"
-                    value="yes"
-                    checked={isUnderTreatment === "yes"}
-                    onChange={setMedicalHistory}
-                  />
-                  Yes
-                </div>
-              </div>
-              <div className="col-sm-1">
-                <div className="response-no-2">
-                  <input
-                    type="radio"
-                    name="isUnderTreatment"
-                    value="no"
-                    checked={isUnderTreatment === "no"}
-                    onChange={setMedicalHistory}
-                  />
-                  No
-                </div>
-              </div>
-              <div className="col-sm-8">
-                {isUnderTreatment === "yes" &&
-                  (<input
-                  type="text"
-                  onChange={setMedicalHistory}
-                  name="specifiedTreatment"
-                  className="condTextInputCont-2"
-                  placeholder="Condition Being Treated"
-                />)}
-              </div>
-            </div>
-            <div className="row mt-4">
-              <div className="col-sm-8">
-                <p className="questions-3">
-                  Have you ever had serious illness or surgical operation?
-                </p>
-                <br />
-              </div>
-              <div className="col-sm-1">
-                <div className="response-yes-3">
-                  <input
-                    type="radio"
-                    name="hasIllnessOrSurgery"
-                    value="yes"
-                    checked={hasIllnessOrSurgery === "yes"}
-                    onChange={setMedicalHistory}
-                  />
-                  Yes
-                </div>
-              </div>
-              <div className="col-sm-1">
-                <div className="response-no-3">
-                  <input
-                    type="radio"
-                    name="hasIllnessOrSurgery"
-                    value="no"
-                    checked={hasIllnessOrSurgery === "no"}
-                    onChange={setMedicalHistory}
-                  />
-                  No
-                </div>
-              </div>
-            </div>
-            <div className="row mt-4">
-              <div className="col-sm-8">
-                <p className="questions-4">Have you ever been hospitalize?</p>
-                <br />
-              </div>
-              <div className="col-sm-1">
-                <div className="response-yes-4">
-                  <input
-                    type="radio"
-                    name="isHospitalized"
-                    value="yes"
-                    checked={isHospitalized === "yes"}
-                    onChange={setMedicalHistory}
-                  />
-                  Yes
-                </div>
-              </div>
-              <div className="col-sm-1">
-                <div className="response-no-4">
-                  <input
-                    type="radio"
-                    name="isHospitalized"
-                    value="no"
-                    checked={isHospitalized === "no"}
-                    onChange={setMedicalHistory}
-                  />
-                  No
-                </div>
-              </div>
-            </div>
-            <div className="row mt-4">
-              <div className="col-sm-8">
-                <p className="questions-5">
-                  Are you taking any prescription/non-prescription medication?
-                </p>
-                <br />
-              </div>
-              <div className="col-sm-1">
-                <div className="response-yes-5">
-                  <input
-                    type="radio"
-                    name="isTakingPrescription"
-                    value="yes"
-                    checked={isTakingPrescription === "yes"}
-                    onChange={setMedicalHistory}
-                  />
-                  Yes
-                </div>
-              </div>
-              <div className="col-sm-1">
-                <div className="response-no-5">
-                  <input
-                    type="radio"
-                    name="isTakingPrescription"
-                    value="no"
-                    checked={isTakingPrescription === "no"}
-                    onChange={setMedicalHistory}
-                  />
-                  No
-                </div>
-              </div>
-              <div className="col-sm-8">
-                {isTakingPrescription === "yes" &&
-                  (<input
-                  type="text"
-                  onChange={setMedicalHistory}
-                  name="specifiedPrescription"
-                  className="condTextInputCont-5"
-                  placeholder="Please Specify"
-                />)}
-              </div>
-            </div>
-            <div className="row mt-4">
-              <div className="col-sm-8">
-                <p className="questions-6">Do you use Tabacco products?</p>
-                <br />
-              </div>
-              <div className="col-sm-1">
-                <div className="response-yes-6">
-                  <input
-                    type="radio"
-                    name="isUsingTabacco"
-                    value="yes"
-                    checked={isUsingTabacco === "yes"}
-                    onChange={setMedicalHistory}
-                  />
-                  Yes
-                </div>
-              </div>
-              <div className="col-sm-1">
-                <div className="response-no-6">
-                  <input
-                    type="radio"
-                    name="isUsingTabacco"
-                    value="no"
-                    checked={isUsingTabacco === "no"}
-                    onChange={setMedicalHistory}
-                  />
-                  No
-                </div>
-              </div>
-            </div>
-            <div className="row mt-4">
-              <div className="col-sm-8">
-                <p className="questions-7">
-                  Do you use alcohol, cocaine or other dangerous drugs?
-                </p>
-                <br />
-              </div>
-              <div className="col-sm-1">
-                <div className="response-yes-7">
-                  <input
-                    type="radio"
-                    name="isUsingDangerousDrugs"
-                    value="yes"
-                    checked={isUsingDangerousDrugs === "yes"}
-                    onChange={setMedicalHistory}
-                  />
-                  Yes
-                </div>
-              </div>
-              <div className="col-sm-1">
-                <div className="response-no-7">
-                  <input
-                    type="radio"
-                    name="isUsingDangerousDrugs"
-                    value="no"
-                    checked={isUsingDangerousDrugs === "no"}
-                    onChange={setMedicalHistory}
-                  />
-                  No
-                </div>
-              </div>
-            </div>
-            <div className="row mt-4">
-              <div className="col-sm-8">
-                <p className="questions-8">
-                  Are you allergic to any of the following (Local Anesthetic,
-                  Penicillin, Antibiotics, Sulfa drugs, Aspirin, Latex,
-                  Others...)?
-                </p>
-                <p></p>
-                <br />
-              </div>
-              <div className="col-sm-1">
-                <div className="response-yes-8">
-                  <input
-                    type="radio"
-                    name="isAllergictoMentionedDrugs"
-                    value="yes"
-                    checked={isAllergictoMentionedDrugs === "yes"}
-                    onChange={setMedicalHistory}
-                  />
-                  Yes
-                </div>
-              </div>
-              <div className="col-sm-1">
-                <div className="response-no-8">
-                  <input
-                    type="radio"
-                    name="isAllergictoMentionedDrugs"
-                    value="no"
-                    checked={isAllergictoMentionedDrugs === "no"}
-                    onChange={setMedicalHistory}
-                  />
-                  No
-                </div>
-              </div>
-              <div className="col-sm-8">
-                {isAllergictoMentionedDrugs === "yes" &&
-                  (<input
-                  type="text"
-                  onChange={setMedicalHistory}
-                  //value={amountPaid}
-                  name="specifiedAllergies"
-                  className="condTextInputCont-8"
-                  placeholder="Please Specify"
-                />)}
-              </div>
-            </div>
-            <div className="row">{proceed()}</div>
-          </div>
+        <div className="medical-form-card-cont">
+          <div className="medical-info-header">Medical History Information</div>
+          {renderMedicalHistoryInformationSection()}
+          {renderCurrentConditionSection()}
+          {renderButtons()}
         </div>
       </div>
     </div>
